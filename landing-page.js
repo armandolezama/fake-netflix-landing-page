56365b0c8177078e1567d2fe66f394fd7815d635
armandolezama <armando.lezama.psic@gmail.com>
2019-12-02 23:50:52 -0600

    Se añadieron componentes para trabajar

--- landing-page.js
+++ landing-page.js
@@ -2,16 +2,21 @@ import { LitElement, css, html } from 'lit-element';
 
 export class LandingPage extends LitElement {
 
+    constructor() {
+        super();
+        this.logged = false;
+    }
     static get styles() {
         return css `
-        #main-page {
+        #first-card {
             background-image: url('https://assets.nflxext.com/ffe/siteui/vlv3/9c5c4787-c6f7-4db7-81b8-280107a1bf36/03152a25-43d0-48db-b503-93abc7ae20c7/MX-es-20191118-popsignuptwoweeks-perspective_alpha_website_medium.jpg'); 
-            heigth: 900px;
+            height: 628px;
+            background-size: cover;
         }
 
         #header {
             display: flex;
-            heigth: 36px;
+            height: 36px;
         }
 
         #logo-section {
@@ -24,27 +29,29 @@ export class LandingPage extends LitElement {
 
         #logo {
               width: 108px;
-              heigth: 32px;
+              height: 32px;
               padding: 8px; 
           }
 
         #login {
             background-color: #e50914;
             width: 140px;
-            heigth: 35px;
+            height: 35px;
             padding: 7px 17px;
             font-weight: 400;
             font-size: 1rem;
             color: #ffffff;
             margin-top: 0.5rem;
             border-radius: 10px;
+            border-color: #e50914;
           }
 
           #first-add {
               color: #ffffff;
-              heigth: 268px;
+              height: 268px;
               width: 640px;
               margin: 0 auto;
+              text-align: center;
           }
 
           #main-add {
@@ -59,7 +66,7 @@ export class LandingPage extends LitElement {
             display: block;
             background-color: #e50914;
             width: 209px;
-            heigth: 44px;
+            height: 44px;
             padding: 7px 17px;
             font-weight: 400;
             font-size: 1rem;
@@ -67,6 +74,12 @@ export class LandingPage extends LitElement {
             margin-top: 0.5rem;
             border-radius: 10px;
             margin: 0 auto;
+            border-color: #e50914;
+          }
+
+          #second-add {
+              width: 760px;
+              height: 672px;
           }
         `;
     }
@@ -82,22 +95,22 @@ export class LandingPage extends LitElement {
     render() {
         return html `
         <section id="main-page">
-            <div id="header">
-                <div id="logo-section">
-                    <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png">  
+            <section id="first-card">
+                <div id="header">
+                    <div id="logo-section">
+                        <img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png">  
+                    </div>
+    
+                    <div id="login-section">
+                        <button id="login" @click="${this.logginLogout}">Iniciar sesión</button>
+                    </div>
                 </div>
-
-                <div id="login-section">
-                    <button id="login">Iniciar sesión</button>
-                </div>
-            </div>
-            <div id="main-container">
                 <article id="first-add">
                     <h1 id="main-add">Programas y películas sin límite y mucho más.</h1>
                     <h2 id="secondary-add">Disfruta donde quieras. Cancela en cualquier momento.</h2>
                     <button id="start-button">Comenzar Ahora ></button>
                 </article>
-            </div>
+            </section>
         </section>
         `;
     }
